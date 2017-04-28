da187a0ae4df5aa4ee7a368248119fad9937cbe0
kimlan nguyen <kimlan1510@yahoo.com>
2017-04-28 09:44:02 -0700

    add 5 vars into scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $("form").submit(function(event){
+    var style = $("#style").val();
+    var environment = $("#environment").val();
+    var company = $("#company").val();
+    var app-type = $("#app-type").val();
+    var person = $("#personality").val();
+  });
+});
